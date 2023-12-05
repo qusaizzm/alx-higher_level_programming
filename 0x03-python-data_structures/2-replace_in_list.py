3d2be60f4056ac32e0062032f652af6f42d9a5b4
root <qusai_zzm@outlook.com>
2023-12-04 22:14:47 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -13,10 +13,9 @@ def replace_in_list(my_list, idx, element):
     """
 
     # Check for negative and out of range index
-    if idx < 0:
+    if idx < 0 or idx >= len(my_list):
         return my_list
-    if idx >= len(my_list):
-        return my_list
+
     my_list[idx] = element
     return my_list
 
