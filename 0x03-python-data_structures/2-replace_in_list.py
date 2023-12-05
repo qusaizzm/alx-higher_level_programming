ca573aa4b2436cb3cac8a427a4f61cb56f7ae66b
root <qusai_zzm@outlook.com>
2023-12-04 21:57:47 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+
+# (C) 2023 Qusai alismat, Sudan
+# email igozeco120@gmail.com
+# -----------------------------------------------------------
+
+
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list) - 1:
+    """Replaces an element of a list at a specific position
+        my_list: a list
+    """
+
+    # Check for negative and out of range index
+    if idx < 0:
         return my_list
-    else:
-        my_list[idx] = element
+    if idx >= len(my_list):
         return my_list
+    my_list[idx] = element
+    return my_list
 
