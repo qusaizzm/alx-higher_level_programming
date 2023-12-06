81be811d5937901abd73d9254bec3cfb2c78cb33
root <qusai_zzm@outlook.com>
2023-12-06 09:20:58 -0800

    0x04 python

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    def s_r_elm(elm):
-        return (elm if elm != search else replace)
-    return list(map(s_r_elm, my_list))
-
+    return (list(map(lambda x: replace if x is search else x, my_list)))
